7f295113d1a9cc4bdd1d4bb3d406ccfc669aba78
huylesitdn <mrsjiodev@gmail.com>
2022-12-10 00:38:39 +0700

    desktop

--- src/assets/js/vendor.js
+++ src/assets/js/vendor.js
@@ -1,8 +1,17 @@
 $('#fullPage').fullpage({
   sectionSelector: '.item',
   // slideSelector: '.horizontal-scrolling',
-  controlArrows: false
+  controlArrows: false,
   // more options here
+  onLeave: function(index, nextIndex, direction){
+    const has_cloud = $('.main').hasClass('has-cloud');
+    if(nextIndex === 3 && !!has_cloud) {
+      $('.main').removeClass('has-cloud')
+    } else {
+      $('.main').addClass('has-cloud')
+    }
+    // console.log(index, nextIndex, direction)
+  },
 });
 
 console.log('====vendor')
